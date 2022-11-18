dc5a168992222963a3d2e0f8a816ed32967e236e
fiction13 <fiction13@yandex.ru>
2022-11-18 21:27:18 +0300

    Fix image generate

--- plg_radicalmicro_image/script.php
+++ plg_radicalmicro_image/script.php
@@ -11,52 +11,221 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Filesystem\File;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Filesystem\Path;
 use Joomla\CMS\Installer\Installer;
 use Joomla\CMS\Installer\InstallerAdapter;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
 
 class PlgRadicalMicroImageInstallerScript
 {
-    /**
-     * Runs right after any installation action.
-     *
-     * @param   string            $type    Type of PostFlight action. Possible values are:
-     * @param   InstallerAdapter  $parent  Parent object calling object.
-     *
-     * @return  boolean True on success, False on failure.
-     *
-     * @throws  Exception
-     *
-     * @since   __DEPLOY_VERSION__
-     */
-    function postflight($type, $parent)
-    {
-        // Enable plugin
-        if ($type == 'install')
+	/**
+	 * Runs right after any installation action.
+	 *
+	 * @param   string            $type    Type of PostFlight action. Possible values are:
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return  boolean True on success, False on failure.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	function postflight($type, $parent)
+	{
+		// Enable plugin
+		if ($type == 'install')
         {
             $this->enablePlugin($parent);
+            $this->generateKey();
         }
 
-        return true;
+        // Parse layouts
+		$this->parseLayouts($parent->getParent()->getManifest()->layouts, $parent->getParent());
+
+		return true;
+	}
+
+	/**
+	 * Enable plugin after installation.
+	 *
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function enablePlugin($parent)
+	{
+		// Prepare plugin object
+		$plugin          = new stdClass();
+		$plugin->type    = 'plugin';
+		$plugin->element = $parent->getElement();
+		$plugin->folder  = (string) $parent->getParent()->manifest->attributes()['group'];
+		$plugin->enabled = 1;
+
+		// Update record
+		Factory::getDbo()->updateObject('#__extensions', $plugin, array('type', 'element', 'folder'));
+	}
+
+    /**
+	 * Generate key after installation.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+    protected function generateKey()
+    {
+        $db = Factory::getDbo();
+
+        // Get params
+        $params = $db->setQuery(
+            $db->getQuery(true)
+                ->select($db->quoteName('params'))
+                ->from($db->quoteName('#__extensions'))
+                ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))
+                ->where($db->quoteName('folder') . ' = ' . $db->quote('radicalmicro'))
+                ->where($db->quoteName('element') . ' = ' . $db->quote('image'))
+        )->loadResult();
+
+        $params = json_decode($params, true);
+
+        // Set secret key
+
+        if (!isset($params['imagetype_generate_secret_key']))
+        {
+            $params['imagetype_generate_secret_key'] = uniqid(rand());
+
+            // Prepare plugin object
+            $plugin          = new stdClass();
+            $plugin->type    = 'plugin';
+            $plugin->element = $parent->getElement();
+            $plugin->folder  = (string) $parent->getParent()->manifest->attributes()['group'];
+            $plugin->params  = json_encode($params);
+
+            // Update
+            Factory::getDbo()->updateObject('#__extensions', $plugin, array('type', 'element', 'folder'));
+        }
     }
 
     /**
-     * Enable plugin after installation.
-     *
-     * @param   InstallerAdapter  $parent  Parent object calling object.
-     *
-     * @since   __DEPLOY_VERSION__
-     */
-    protected function enablePlugin($parent)
-    {
-        // Prepare plugin object
-        $plugin           = new stdClass();
-        $plugin->type     = 'plugin';
-        $plugin->element  = $parent->getElement();
-        $plugin->folder   = (string) $parent->getParent()->manifest->attributes()['group'];
-        $plugin->ordering = 1000;
-        $plugin->enabled  = 1;
+	 * Method to parse through a layout element of the installation manifest and take appropriate action.
+	 *
+	 * @param   SimpleXMLElement  $element    The XML node to process.
+	 * @param   Installer         $installer  Installer calling object.
+	 *
+	 * @return  boolean     True on success
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public function parseLayouts(SimpleXMLElement $element, $installer)
+	{
+		if (!$element || !count($element->children()))
+		{
+			return false;
+		}
 
-        // Update record
-        Factory::getDbo()->updateObject('#__extensions', $plugin, array('type', 'element', 'folder'));
-    }
+		// Get destination
+		$folder      = ((string) $element->attributes()->destination) ? '/' . $element->attributes()->destination : null;
+		$destination = Path::clean(JPATH_ROOT . '/layouts' . $folder);
+
+		// Get source
+		$folder = (string) $element->attributes()->folder;
+		$source = ($folder && file_exists($installer->getPath('source') . '/' . $folder)) ?
+			$installer->getPath('source') . '/' . $folder : $installer->getPath('source');
+
+		// Prepare files
+		$copyFiles = array();
+		foreach ($element->children() as $file)
+		{
+			$path['src']  = Path::clean($source . '/' . $file);
+			$path['dest'] = Path::clean($destination . '/' . $file);
+
+			// Is this path a file or folder?
+			$path['type'] = $file->getName() === 'folder' ? 'folder' : 'file';
+			if (basename($path['dest']) !== $path['dest'])
+			{
+				$newdir = dirname($path['dest']);
+				if (!Folder::create($newdir))
+				{
+					Log::add(Text::sprintf('JLIB_INSTALLER_ERROR_CREATE_DIRECTORY', $newdir), Log::WARNING, 'jerror');
+
+					return false;
+				}
+			}
+
+			$copyFiles[] = $path;
+		}
+
+		return $installer->copyFiles($copyFiles);
+	}
+
+	/**
+	 * This method is called after extension is uninstalled.
+	 *
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @return  void
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public function uninstall($parent)
+	{
+		// Remove layouts
+		$this->removeLayouts($parent->getParent()->getManifest()->layouts);
+	}
+
+	/**
+	 * Method to parse through a layouts element of the installation manifest and remove the files that were installed.
+	 *
+	 * @param   SimpleXMLElement  $element  The XML node to process.
+	 *
+	 * @return  boolean     True on success
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected function removeLayouts(SimpleXMLElement $element)
+	{
+		if (!$element || !count($element->children()))
+		{
+			return false;
+		}
+
+        $retval = true;
+
+		// Get the array of file nodes to process
+		$files = $element->children();
+
+		// Get source
+		$folder = ((string) $element->attributes()->destination) ? '/' . $element->attributes()->destination : null;
+		$source = Path::clean(JPATH_ROOT . '/layouts' . $folder);
+
+		// Process each file in the $files array (children of $tagName).
+		foreach ($files as $file)
+		{
+			$path = Path::clean($source . '/' . $file);
+
+			// Actually delete the files/folders
+			if (is_dir($path))
+			{
+				$val = Folder::delete($path);
+			}
+			else
+			{
+				$val = File::delete($path);
+			}
+
+			if ($val === false)
+			{
+				Log::add('Failed to delete ' . $path, Log::WARNING, 'jerror');
+				$retval = false;
+			}
+		}
+
+		if (!empty($folder))
+		{
+			Folder::delete($source);
+		}
+
+		return $retval;
+	}
 }
---
+++ plg_radicalmicro_image/src/Fields/ImagepreviewField.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * @package   pkg_radicalmicro
+ * @version   __DEPLOY_VERSION__
+ * @author    Dmitriy Vasyukov - https://fictionlabs.ru
+ * @copyright Copyright (c) 2022 Fictionlabs. All rights reserved.
+ * @license   GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link      https://fictionlabs.ru/
+ */
+
+namespace RadicalMicro\Addon\Image\Fields;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Plugin\PluginHelper;
+use RadicalMicro\Helpers\UtilityHelper;
+
+class ImagepreviewField extends FormField
+{
+    /**
+     * @var array
+     *
+     * @since __DEPLOY_VERSION__
+     */
+    protected $params = [];
+
+    /**
+     * The form field type.
+     *
+     * @var  string
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $type = 'imagepreview';
+
+    /**
+     * Name of the layout being used to render the field
+     *
+     * @var    string
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $layout = 'plugins.radicalmicro.image.fields.imagepreview';
+
+    /**
+     * Hide the label when rendering the form field.
+     *
+     * @var    boolean
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $hiddenLabel = true;
+
+    /**
+	 * Method to instantiate the form field object.
+	 *
+	 * @param   Form  $form  The form to attach to the form field object.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+    public function __construct($form = null)
+    {
+        parent::__construct($form);
+
+        Factory::getApplication()->getLanguage()->load('plg_system_radicalmicro', JPATH_ADMINISTRATOR, null, true);
+    }
+
+    /**
+     * Method to get the field input markup fora grouped list.
+     * Multiselect is enabled by using the multiple attribute.
+     *
+     * @return  string  The field input markup.
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected function getInput()
+    {
+        $data             = $this->getLayoutData();
+        $data['isEnabled'] = $this->checkPlugin();
+
+        return $this->getRenderer($this->layout)->render($data);
+    }
+
+    /**
+     * Method to check Radical Micro plugin enable
+     *
+     * @return  string  The field input markup.
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected function checkPlugin()
+    {
+        return PluginHelper::isEnabled('system', 'radicalmicro');
+    }
+}
